5615be22c8a50065c06bdcb12945b9c2d7e9b25f
Ingrid Pettersson <ingrid.s.pettersson@gmail.com>
2021-01-20 10:57:06 +0100

    add the projects

---
+++ AWADay3/AWADay3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AWADay3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Modulus in game
+
+            bool done = false;
+            int round = 1;
+
+            while (!done)
+            {
+                Console.WriteLine($"Round {round}: Player {(round % 4) + 1} shall roll the dice.");
+                round++;
+                done = round > 25;
+                Console.ReadKey(true);
+            }
+
+
+            //double a = 10.10;
+            //double b = 3.30;
+            //double rest = a % b;
+
+            //Console.WriteLine(rest);
+        }
+    }
+}
---
+++ AWADay3/AWAPractice401/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AWAPractice401
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
